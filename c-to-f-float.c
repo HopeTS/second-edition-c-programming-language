a8926471dbfd87fbcc26d5a1f881609cb6eef137
Robby Bergers <bergersr97@gmail.com>
2022-09-07 19:45:09 -0400

    1.2 floating point fahrenheit to celsius

---
+++ c-to-f-float.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Print fahrenheit-Celsius table */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /* Lower limit of temp. table 8 */
+    upper = 300; /* Upper limit 8 */
+    step = 10;   /* Step size */
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
