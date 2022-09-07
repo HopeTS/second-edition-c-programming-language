3f68ab2834207d4070503335360a951ca2cd922b
Robby Bergers <bergersr97@gmail.com>
2022-09-07 06:42:53 -0400

    fix return type?

--- chapter-1/c-to-f.c
+++ chapter-1/c-to-f.c
@@ -3,7 +3,7 @@
 /*
  * print Fahrenheit-Celsuis table for F = 0, 20, ..., 300
  */
-main()
+void main()
 {
     int fahr, celsius;
     int lower, upper, step;
@@ -19,4 +19,6 @@ main()
         printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
+
+    return
 }
