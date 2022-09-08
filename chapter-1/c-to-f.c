005e0e7e6f7dd368dd40b05c872fe39b07ebf35f
Robby Bergers <bergersr97@gmail.com>
2022-09-07 20:12:53 -0400

    create celsius to fahrenheit conversion table

---
+++ chapter-1/c-to-f.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** Print Celsius-Fahrenheit table */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /* Lower C limit */
+    upper = 300; /* Upper C limit */
+    step = 10;   /* Step size */
+
+    celsius = lower;
+    printf("Celsius to Fahrenheit conversion table\n");
+    while (celsius <= upper)
+    {
+        fahr = (celsius / (5.0 / 9.0)) + 32;
+        printf("%3.0f\t%3.2f", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
