7de918dc752a439330aeea66760026613a912060
Robby Bergers <bergersr97@gmail.com>
2022-09-07 20:36:19 -0400

    exercise 1-5 reverse conversion table

---
+++ chapter-1/1-5-reverse-conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** Reverse Print Fahrenheit-Celsius table */
+{
+    int lower, upper, step;
+
+    lower = 0;   /* Lower F limit */
+    upper = 300; /* Upper F limit */
+    step = 10;   /* Step size */
+
+    printf("Fahrenheit to Celsius conversion table\n");
+    for (fahr = upper; fahr >= lower; fahr = fahr - step)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+}
