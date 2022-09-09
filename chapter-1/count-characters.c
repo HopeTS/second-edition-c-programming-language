d69cebc22bc4b44d8fae65c5964fcb21f557966a
Robby Bergers <bergersr97@gmail.com>
2022-09-09 05:58:30 -0400

    version 1 of count characters

---
+++ chapter-1/count-characters.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* version 1 of count characters function */
+version1()
+{
+    long nc;
+
+    nc = 0;
+    while (getchar() != EOF)
+    {
+        ++nc;
+    }
+
+    printf("%1d\n", nc);
+}
+
+main()
+{
+    version1()
+}
