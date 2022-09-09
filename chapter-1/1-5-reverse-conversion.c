7c91d772a3d7c1427d2e4bd955fe6e5aeb97cc08
Robby Bergers <bergersr97@gmail.com>
2022-09-08 20:14:50 -0400

    use define for constant aliasing

--- chapter-1/1-5-reverse-conversion.c
+++ chapter-1/1-5-reverse-conversion.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0   /* Lower F limit */
+#define UPPER 300 /* Upper F limit */
+#define STEP 10   /* Step size */
+
 /** Reverse Print Fahrenheit-Celsius table */
 main()
 {
-    int lower, upper, step;
-
-    lower = 0;   /* Lower F limit */
-    upper = 300; /* Upper F limit */
-    step = 10;   /* Step size */
-
     printf("Fahrenheit to Celsius conversion table\n");
-    for (int fahr = upper; fahr >= lower; fahr = fahr - step)
+    for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
