77779e4ae5855b9ed473aaf2efd3f7af0ac8cf56
Robby Bergers <bergersr97@gmail.com>
2022-09-11 20:26:52 -0400

    word count example from book

---
+++ chapter-1/word-counting.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define IN 1  /* Inside a word */
+#define OUT 2 /* Outside a word */
+
+/* Count lines, words and characters in input */
+main()
+{
+    int c;     /* Current character */
+    int nl;    /* Line count */
+    int nw;    /* Word count */
+    int nc;    /* Character count */
+    int state; /* Inside or outside of a word? */
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c - getchar()) != EOF)
+    {
+        ++nc;
+
+        if (c == '\n')
+        {
+            ++nl;
+        }
+
+        if (c-- ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+        }
+
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("Line count: %d | Word count: %d | Character count: %d", nl, nw, nc);
+}
